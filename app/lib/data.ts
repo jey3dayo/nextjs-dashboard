b3c9ece6dc9fdce60f913a15664b9241e9715bf9
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2024-03-14 14:06:30 +0900

    feat: Add authentication functionality and login page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -239,7 +239,7 @@ export async function fetchFilteredCustomers(query: string) {
   }
 }
 
-export async function getUser(email: string) {
+export async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0] as User;
